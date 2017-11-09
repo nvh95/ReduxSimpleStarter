62f6215468537499707698ddb3ec7c5f2aaef9ce
henry <henry@gotitapp.co>
2017-11-09 20:23:01 +0700

    Finished module 1 of the tutorial

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component} from 'react';
 
-const SearchBar = () => {
-    return <input type="text"/>
-};
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+	}
+	render() {
+		return (
+		<div>
+			<input onChange={event => this.setState({term: event.target.value})} />
+			Value of the input: {this.state.term}
+		</div>);
+	}
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAUHF8UwzBkFBIyQlqWUaxWGjzL5aqdoho';
 
 // Create a new component. This should produce some HTML
 const App =  () => {
-    return <div>Hi!</div>
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
